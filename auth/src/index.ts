500d0e92995f0bd0a00acf8396ac36c30bf13864
altegani mohammed <tiganimoh@gmail.com>
2020-09-12 17:48:10 +0200

    Response Normalization Strategies

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,14 +1,27 @@
 import express from 'express';
+import 'express-async-errors';
 import {json} from 'body-parser';
-
+import { currenUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/sigin';
+import { signoutRouter } from './routes/siginout';
+import { signUpRouter } from './routes/siginup';
+import {errorHandler} from './middlewares/error-handler';
+import {NotFoundError} from './errors/not-found-error';
 
 const app = express();
-
 app.use(json());
 
-app.get('/api/users/currentuser',(req,res) =>{
-    res.send('Hi there')
-})
+app.use(currenUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signUpRouter);
+app.use(errorHandler);
+
+app.all('*',async(req,res)=>{
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000,()=>{
     console.log("listeng to 3000");
