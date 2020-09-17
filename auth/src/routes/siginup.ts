ff0ced120540bc0d8abc77d501421cbee9788a1e
altegani mohammed <tiganimoh@gmail.com>
2020-09-17 19:41:54 +0200

    Authentication Strategies and Options

--- auth/src/routes/siginup.ts
+++ auth/src/routes/siginup.ts
@@ -1,8 +1,9 @@
 import express,{Request,Response} from 'express';
-import {body,validationResult} from 'express-validator';
+import {body} from 'express-validator';
 import {User} from '../models/user';
-import {RequestValidationError} from '../errors/request-validation-error';
 import {BadRequestError} from '../errors/bad-request-error';
+import jwt from 'jsonwebtoken';
+import {validationRequest} from '../middlewares/validate-request';
 
 const router = express.Router();
 
@@ -14,13 +15,8 @@ router.post('/api/users/signup',[
       .trim()  
       .isLength({ min:4,max:20})
       .withMessage('password must be between 4 and 20 chcarcter')
-],
+],validationRequest,
 async(req:Request,res:Response) => {
-   const errors = validationResult(req);
-   if(!errors.isEmpty()){
-      throw new RequestValidationError(errors.array());
-   }
-
    const {email,password} = req.body;
    const existingUser = await User.findOne({email})
    
@@ -30,6 +26,17 @@ async(req:Request,res:Response) => {
 
    const user = User.build({email,password});
    await user.save();
+
+   //Generate JWT
+   const userJwt = jwt.sign({
+      id:user.id,
+      email:user.email
+   },process.env.JWT_KEY!);
+
+   //Store it on session object
+    req.session = {
+       jwt:userJwt
+    };
 
    res.status(201).send(user);
 });
