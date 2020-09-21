66679b38d7ed2c966c18db5621412f15f8096725
altegani mohammed <tiganimoh@gmail.com>
2020-09-21 13:14:25 +0200

    Testing Isolated Microservices

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,36 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-import {json} from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSeaaion from 'cookie-session';
-import { currenUserRouter } from './routes/current-user';
-import { signInRouter } from './routes/siginin';
-import { signoutRouter } from './routes/siginout';
-import { signUpRouter } from './routes/siginup';
-import {errorHandler} from './middlewares/error-handler';
-import {NotFoundError} from './errors/not-found-error';
-
-const app = express();
-app.set('trust proxy',true);
-app.use(json());
-app.use(
-  cookieSeaaion({
-    signed:false,
-    secure:true
-  })
-)
-
-app.use(currenUserRouter);
-app.use(signInRouter);
-app.use(signoutRouter);
-app.use(signUpRouter);
-app.use(errorHandler);
-
-app.all('*',async(req,res)=>{
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import {app} from './app'
 
 const start = async()=>{
   if(!process.env.JWT_KEY){
