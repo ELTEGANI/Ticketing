5ef3d081c806fdf7fa5e45e229086d0bfd02697c
altegani mohammed <tiganimoh@gmail.com>
2020-09-26 11:44:24 +0200

    Code Sharing and Reuse Between Services

--- auth/src/routes/siginin.ts
+++ auth/src/routes/siginin.ts
@@ -1,8 +1,7 @@
 import express ,{Request,Response} from 'express';
 import {body} from 'express-validator';
-import {validationRequest} from '../middlewares/validate-request';
+import {validationRequest,BadRequestError} from '@tjtickets/common';
 import {User} from '../models/user';
-import {BadRequestError} from '../errors/bad-request-error';
 import {Password} from '../utils/password';
 import jwt from 'jsonwebtoken';
 
